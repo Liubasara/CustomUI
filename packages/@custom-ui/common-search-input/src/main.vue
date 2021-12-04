1a303515d668133faccc365dc99d18deb7064092
liubasara <followliu@163.com>
2021-12-04 12:10:24 +0800

    feat: template add

--- packages/@custom-ui/common-search-input/src/main.vue
+++ packages/@custom-ui/common-search-input/src/main.vue
@@ -1,17 +1,20 @@
 <template>
-  <div>
-    JSTemplate
-  </div>
+  <div class="js-template">{{ JSTemplate }}</div>
 </template>
 
 <script>
 export default {
   setup() {
-    
+    return {
+      JSTemplate: 'JS Template'
+    }
   }
 }
 </script>
 
 <style lang="less">
-
+.js-template {
+  background: #000;
+  color: blue;
+}
 </style>
--- packages/@custom-ui/frame-select/src/main.vue
+++ packages/@custom-ui/frame-select/src/main.vue
@@ -1,16 +1,21 @@
 <template>
-  <div>
-    TSTemplate
-  </div>
+  <div class="ts-template">{{ TSTemplate }}</div>
 </template>
 
 <script lang="ts">
-export default {
+import { defineComponent } from 'vue'
+export default defineComponent({
   setup() {
-
+    return {
+      TSTemplate: 'my ts template'
+    }
   }
-}
+})
 </script>
 
 <style lang="less">
+.ts-template {
+  background: #000;
+  color: red;
+}
 </style>
